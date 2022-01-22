807c4a6d607fc09d3a61e551c3070589ab136745
hjw0703 <hjw0703@student.ubc.ca>
2022-01-21 17:34:51 -0800

    Function missing imputer docstring

---
+++ R/missing_imputer.R
@@ -0,0 +1,13 @@
+#' Return the imputed version of data based on the methods selected
+#'
+#' @param data the raw data with missing values
+#' @param method the method used to impute the missing values
+#'
+#' @return an imputed dataframe with the method specified
+#' @export
+#'
+#' @examples
+#' missing_imputer(data , method = "median")
+missing_imputer <- function(data, method = "mean") {
+  
+}
