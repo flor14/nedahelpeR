d9d2781dcd7650fb95be57dff265343adc20b3ca
Anupriya Srivastava <anupriya.g.srivastava@gmail.com>
2022-01-28 18:57:58 -0800

    add implementation

--- R/get_correlated_features.R
+++ R/get_correlated_features.R
@@ -14,14 +14,38 @@
 #'                 'income'=c(20, 120, 120, 25))
 #' get_correlated_features(X, threshold=0.7)
 #'
-get_correlated_features <- function(X, threshold, consider_sign=FALSE) {
+get_correlated_features <- function(x, threshold, consider_sign=FALSE) {
 
     #Exception handling for input datatypes
-    if (!is.data.frame(X)) {
-        stop("Feature set (X) should be a dataframe!")
+    if (!is.data.frame(x)) {
+        stop("Feature set (x) should be a dataframe!")
     }
     if (typeof(threshold) != "double") {
         stop("Threshold value should be a number!")
     }
-}
+    
+    # create empty dataframe for storing correlated features
+    colClasses = c("character", "character", "double")
+    col.names = c("feature_1", "feature_2", "correlation")
+    corr_df <- read.table(text = "",
+        colClasses = colClasses,
+        col.names = col.names)
 
+    features <- colnames(X)
+    for(feat_1 in features) {
+        for(feat_2 in features) {
+            corr_val_abs <- 0
+            corr_val <- round(cor(x[feat_1], x[feat_2]), 2)      # calculate pair-wise correlation
+            
+            ifelse(consider_sign == FALSE,
+                corr_val_abs <- abs(corr_val),
+                corr_val_abs <- corr_val)
+            
+            if (feat_1 != feat_2 & corr_val_abs >= threshold) {
+                corr_df[nrow(corr_df) + 1,] = c(feat_1, feat_2, corr_val) # Add correlated pairs to dataframe
+            }
+        }
+    }
+    
+    return(corr_df)
+}
