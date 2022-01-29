28df89dba94c22675443611d68c2cd0893491c1b
Anupriya Srivastava <anupriya.g.srivastava@gmail.com>
2022-01-28 20:52:01 -0800

    correct variable name

--- R/get_correlated_features.R
+++ R/get_correlated_features.R
@@ -1,18 +1,21 @@
 #' Calculate correlation between all feature pairs in the numeric dataframe.
 #' Returns feature pairs having correlation higher than the threshold value.
 #'
-#' @param X Input DataFrame or tibble containing numeric variables
-#' @param threshold Float value for correlation above which feature pairs will be returned
-#' @param consider_sign (boolean) determines whether correlation value has to be checked for magnitude
-#'        only or for sign (positive/ negative) also. Default checks only the magnitude.
-#'        
-#' @return DataFrame Tibble containing containing feature1, feature2, and corresponding correlation.
+#' @param x Input DataFrame or tibble containing numeric variables
+#' @param threshold Float value for correlation above 
+#'                which feature pairs will be returned
+#' @param consider_sign (boolean) determines whether correlation value has to
+#'                be checked for magnitude only or for sign 
+#'                (positive/ negative) also. Default checks only the magnitude.
+#'
+#' @return DataFrame Tibble containing containing feature1, feature2,
+#'                and corresponding correlation.
 #'
 #' @examples
-#' X <- data.frame('age'=c(23, 13, 7, 45), 
+#' x <- data.frame('age'=c(23, 13, 7, 45),
 #'                 'height'=c(1.65, 1.23, 0.96, 1.55)
 #'                 'income'=c(20, 120, 120, 25))
-#' get_correlated_features(X, threshold=0.7)
+#' get_correlated_features(x, threshold=0.7)
 #'
 get_correlated_features <- function(x, threshold, consider_sign=FALSE) {
 
@@ -23,7 +26,7 @@ get_correlated_features <- function(x, threshold, consider_sign=FALSE) {
     if (typeof(threshold) != "double") {
         stop("Threshold value should be a number!")
     }
-    
+
     # create empty dataframe for storing correlated features
     colClasses = c("character", "character", "double")
     col.names = c("feature_1", "feature_2", "correlation")
@@ -31,21 +34,23 @@ get_correlated_features <- function(x, threshold, consider_sign=FALSE) {
         colClasses = colClasses,
         col.names = col.names)
 
-    features <- colnames(X)
+    features <- colnames(x)
     for(feat_1 in features) {
         for(feat_2 in features) {
             corr_val_abs <- 0
-            corr_val <- round(cor(x[feat_1], x[feat_2]), 2)      # calculate pair-wise correlation
-            
+                # calculate pair-wise correlation
+                corr_val <- round(cor(x[feat_1], x[feat_2]), 2)
+
             ifelse(consider_sign == FALSE,
                 corr_val_abs <- abs(corr_val),
                 corr_val_abs <- corr_val)
-            
+
+            # Add correlated pairs to dataframe
             if (feat_1 != feat_2 & corr_val_abs >= threshold) {
-                corr_df[nrow(corr_df) + 1,] = c(feat_1, feat_2, corr_val) # Add correlated pairs to dataframe
+                corr_df[nrow(corr_df) + 1, ] <- c(feat_1, feat_2, corr_val)
             }
         }
     }
-    
+
     return(corr_df)
 }
